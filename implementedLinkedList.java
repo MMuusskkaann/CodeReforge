517c1814a31b4220f6068b590fb434ce8279feb3
Muskan Chaudhary <muskanchaudhary152005@gmail.com>
2026-07-09 19:56:19 +0530

    insert at first in linked list

---
+++ implementedLinkedList.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+    }
+}
+
+class LinkedList{
+    Node head;
+    Node tail;
+
+    void insertFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail= newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void printLL(){
+        Node temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+}
+public class implementedLinkedList{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        LinkedList ll = new LinkedList();
+
+        System.out.print("Enter the number of elements to insert : ");
+        int n = sc.nextInt();
+
+        for(int i = 0;i<n;i++){
+            ll.insertFirst(sc.nextInt());
+        }
+
+        ll.printLL();
+
+        sc.close();
+    }
+}
